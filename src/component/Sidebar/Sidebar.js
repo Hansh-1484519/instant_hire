31c770a0818c187963b7afea98cb0903e6db8eb5
Hansh Raj Sharma <hanshrajsharmamax360@gmail.com>
2022-06-03 17:51:07 +0530

    updated user's basic info in sidebar component

--- src/component/Sidebar/Sidebar.js
+++ src/component/Sidebar/Sidebar.js
@@ -4,7 +4,13 @@ import './sidebar.css'
 const Sidebar = () => {
     return (
         <div>
-            <h1>user</h1>
+            <div>
+            <img src="https://static.remove.bg/remove-bg-web/6ad52d54336ad62d58e7bd1317d40fb98e377ad5/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg" alt="BigCo Inc. logo"/>
+            </div>
+            <div>
+                <span>Hansh Raj Sharma</span>
+                <p>NIT Raipur</p>
+            </div>
         </div>
     );
 }
