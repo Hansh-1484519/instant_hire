7d53ea2c09d58be1051b41d35c8d4994f8dab3c4
Hansh Raj Sharma <hanshrajsharmamax360@gmail.com>
2022-06-03 18:47:28 +0530

    added jobs component for display jobs info

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Button from './component/Button/Button';
+import Jobs from './component/Jobs/Jobs';
 import Sidebar from './component/Sidebar/Sidebar';
 
 
@@ -9,6 +10,7 @@ function App() {
       <h2>INSTA-HIRE CAREERS</h2>
       <p>what do you want to do?</p>
       <Button/>
+      <Jobs/>
       <Sidebar/>
     </div>
   );
---
+++ src/component/Jobs/Jobs.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './jobs.css'
+
+const Jobs = () => {
+    return (
+        <div>
+            <h1>Job Openings</h1>
+        </div>
+    )
+}
+
+export default Jobs
